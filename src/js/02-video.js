197a9151ea8a1062eee37161e756b27e34bb99ac
nataliakif <nataliakif@gmail.com>
2022-02-16 10:09:09 +0200

    throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,13 @@
 
 import Player from '@vimeo/player';
-
+import throttle from 'lodash.throttle'
 
 const iframe = document.querySelector('iframe');
 const STORAGE_KEY ='videoplayer-current-time';
 
     const player = new Player(iframe);
     populateCurrentTime();
-    player.on('timeupdate', timePicker);
+    player.on('timeupdate', throttle(timePicker,1000));
 
     function timePicker(e) {
         localStorage.setItem(STORAGE_KEY, e.seconds);
